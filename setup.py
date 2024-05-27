87825c5adede01892b63efc4dca90acc3360bd9b
thraneh <thraneh@gmail.com>
2024-05-27 14:30:09 +0200

    update

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ from setuptools import setup
 __version__ = os.environ.get("GIT_DESCRIBE_TAG", "0.0.0")
 
 setup(
-    name="roq",
+    name="roq_samples",
     version=__version__,
     author="Hans Erik Thrane",
     author_email="thraneh@gmail.com",
     url="https://roq-trading.com/",
     description="Roq Python Samples",
     long_description="",
-    ext_modules=ext_modules,
-    extras_require={
-        "test": "pytest",
-    },
     zip_safe=False,
     python_requires=">=3.10",
+    install_requires=[
+        "roq",
+    ],
 )
