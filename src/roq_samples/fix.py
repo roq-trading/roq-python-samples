93505f040000bff24cc90c44d221a7bd3a395698
thraneh <thraneh@gmail.com>
2025-03-04 09:03:58 +0100

    update

--- src/roq_samples/fix.py
+++ src/roq_samples/fix.py
@@ -15,10 +15,10 @@ import roq
 encoder = roq.codec.fix.Encoder(sender_comp_id="me", target_comp_id="you")
 
 logon = roq.codec.fix.Logon(
-    heart_bt_int=timedelta(seconds=30),
+    heart_bt_int=30,  # XXX TODO timedelta(seconds=30),
     username="my_name",
     password="xyz",
-    encrypt_method=roq.codec.fix.EncryptMethod.NONE,
+    encrypt_method=roq.fix.EncryptMethod.NONE,
 )
 
 message = encoder.encode(logon, datetime.now())
@@ -27,113 +27,113 @@ print('message="{}", length={}'.format(message.decode().replace(chr(1), "|"), le
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, logon: roq.codec.fix.Logon):
+def callback(header: roq.fix.Header, logon: roq.codec.fix.Logon):
     print("logon={}, header={}".format(logon, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, logout: roq.codec.fix.Logout):
+def callback(header: roq.fix.Header, logout: roq.codec.fix.Logout):
     print("logout={}, header={}".format(logout, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, test_request: roq.codec.fix.TestRequest):
+def callback(header: roq.fix.Header, test_request: roq.codec.fix.TestRequest):
     print("test_request={}, header={}".format(test_request, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, heartbeat: roq.codec.fix.Heartbeat):
+def callback(header: roq.fix.Header, heartbeat: roq.codec.fix.Heartbeat):
     print("heartbeat={}, header={}".format(heartbeat, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, resend_request: roq.codec.fix.ResendRequest):
+def callback(header: roq.fix.Header, resend_request: roq.codec.fix.ResendRequest):
     print("resend_request={}, header={}".format(resend_request, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, reject: roq.codec.fix.Reject):
+def callback(header: roq.fix.Header, reject: roq.codec.fix.Reject):
     print("reject={}, header={}".format(reject, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, business_message_reject: roq.codec.fix.BusinessMessageReject):
+def callback(header: roq.fix.Header, business_message_reject: roq.codec.fix.BusinessMessageReject):
     print("business_message_reject={}, header={}".format(business_message_reject, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, user_request: roq.codec.fix.UserRequest):
+def callback(header: roq.fix.Header, user_request: roq.codec.fix.UserRequest):
     print("user_request={}, header={}".format(user_request, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, user_response: roq.codec.fix.UserResponse):
+def callback(header: roq.fix.Header, user_response: roq.codec.fix.UserResponse):
     print("user_response={}, header={}".format(user_response, header))
 
 
 @typedispatch
 def callback(
-    header: roq.codec.fix.Header,
+    header: roq.fix.Header,
     trading_session_status_request: roq.codec.fix.TradingSessionStatusRequest,
 ):
     print("trading_session_status_request={}, header={}".format(trading_session_status_request, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, trading_session_status: roq.codec.fix.TradingSessionStatus):
+def callback(header: roq.fix.Header, trading_session_status: roq.codec.fix.TradingSessionStatus):
     print("trading_session_status={}, header={}".format(trading_session_status, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, security_list_request: roq.codec.fix.SecurityListRequest):
+def callback(header: roq.fix.Header, security_list_request: roq.codec.fix.SecurityListRequest):
     print("security_list_request={}, header={}".format(security_list_request, header))
 
 
 @typedispatch
-def callback(header: roq.codec.fix.Header, security_list: roq.codec.fix.SecurityList):
+def callback(header: roq.fix.Header, security_list: roq.codec.fix.SecurityList):
     print("security_list={}, header={}".format(security_list, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header,
+#     header: roq.fix.Header,
 #     security_definition_request: roq.codec.fix.SecurityDefinitionRequest,
 # ):
 #     print("security_definition_request={}, header={}".format(security_definition_request, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, security_definition: roq.codec.fix.SecurityDefinition):
+# def callback(header: roq.fix.Header, security_definition: roq.codec.fix.SecurityDefinition):
 #     print("security_definition={}, header={}".format(security_definition, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, security_status_request: roq.codec.fix.SecurityStatusRequest
+#     header: roq.fix.Header, security_status_request: roq.codec.fix.SecurityStatusRequest
 # ):
 #     print("security_status_request={}, header={}".format(security_status_request, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, security_status: roq.codec.fix.SecurityStatus):
+# def callback(header: roq.fix.Header, security_status: roq.codec.fix.SecurityStatus):
 #     print("security_status={}, header={}".format(security_status, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, market_data_request: roq.codec.fix.MarketDataRequest):
+# def callback(header: roq.fix.Header, market_data_request: roq.codec.fix.MarketDataRequest):
 #     print("market_data_request={}, header={}".format(market_data_request, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, market_data_request_reject: roq.codec.fix.MarketDataRequestReject
+#     header: roq.fix.Header, market_data_request_reject: roq.codec.fix.MarketDataRequestReject
 # ):
 #     print("market_data_request_reject={}, header={}".format(market_data_request_reject, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header,
+#     header: roq.fix.Header,
 #     market_data_snapshot_full_refresh: roq.codec.fix.MarketDataSnapshotFullRefresh,
 # ):
 #     print(
@@ -145,7 +145,7 @@ def callback(header: roq.codec.fix.Header, security_list: roq.codec.fix.Security
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header,
+#     header: roq.fix.Header,
 #     market_data_incremental_refresh: roq.codec.fix.MarketDataIncrementalRefresh,
 # ):
 #     print(
@@ -156,30 +156,30 @@ def callback(header: roq.codec.fix.Header, security_list: roq.codec.fix.Security
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, order_status_request: roq.codec.fix.OrderStatusRequest):
+# def callback(header: roq.fix.Header, order_status_request: roq.codec.fix.OrderStatusRequest):
 #     print("order_status_request={}, header={}".format(order_status_request, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, order_mass_status_request: roq.codec.fix.OrderMassStatusRequest
+#     header: roq.fix.Header, order_mass_status_request: roq.codec.fix.OrderMassStatusRequest
 # ):
 #     print("order_mass_status_request={}, header={}".format(order_mass_status_request, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, new_order_single: roq.codec.fix.NewOrderSingle):
+# def callback(header: roq.fix.Header, new_order_single: roq.codec.fix.NewOrderSingle):
 #     print("new_order_single={}, header={}".format(new_order_single, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, order_cancel_request: roq.codec.fix.OrderCancelRequest):
+# def callback(header: roq.fix.Header, order_cancel_request: roq.codec.fix.OrderCancelRequest):
 #     print("order_cancel_request={}, header={}".format(order_cancel_request, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header,
+#     header: roq.fix.Header,
 #     order_cancel_replace_request: roq.codec.fix.OrderCancelReplaceRequest,
 # ):
 #     print("order_cancel_replace_request={}, header={}".format(order_cancel_replace_request, header))
@@ -187,50 +187,50 @@ def callback(header: roq.codec.fix.Header, security_list: roq.codec.fix.Security
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, order_mass_cancel_request: roq.codec.fix.OrderMassCancelRequest
+#     header: roq.fix.Header, order_mass_cancel_request: roq.codec.fix.OrderMassCancelRequest
 # ):
 #     print("order_mass_cancel_request={}, header={}".format(order_mass_cancel_request, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, order_cancel_reject: roq.codec.fix.OrderCancelReject):
+# def callback(header: roq.fix.Header, order_cancel_reject: roq.codec.fix.OrderCancelReject):
 #     print("order_cancel_reject={}, header={}".format(order_cancel_reject, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, execution_report: roq.codec.fix.ExecutionReport):
+# def callback(header: roq.fix.Header, execution_report: roq.codec.fix.ExecutionReport):
 #     print("execution_report={}, header={}".format(execution_report, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header,
+#     header: roq.fix.Header,
 #     trade_capture_report_request: roq.codec.fix.TradeCaptureReportRequest,
 # ):
 #     print("trade_capture_report_request={}, header={}".format(trade_capture_report_request, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, trade_capture_report: roq.codec.fix.TradeCaptureReport):
+# def callback(header: roq.fix.Header, trade_capture_report: roq.codec.fix.TradeCaptureReport):
 #     print("trade_capture_report={}, header={}".format(trade_capture_report, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, request_for_positions: roq.codec.fix.RequestForPositions
+#     header: roq.fix.Header, request_for_positions: roq.codec.fix.RequestForPositions
 # ):
 #     print("request_for_positions={}, header={}".format(request_for_positions, header))
 
 
 # @typedispatch
 # def callback(
-#     header: roq.codec.fix.Header, request_for_positions_ack: roq.codec.fix.RequestForPositionsAck
+#     header: roq.fix.Header, request_for_positions_ack: roq.codec.fix.RequestForPositionsAck
 # ):
 #     print("request_for_positions_ack={}, header={}".format(request_for_positions_ack, header))
 
 
 # @typedispatch
-# def callback(header: roq.codec.fix.Header, position_report: roq.codec.fix.PositionReport):
+# def callback(header: roq.fix.Header, position_report: roq.codec.fix.PositionReport):
 #     print("position_report={}, header={}".format(position_report, header))
 
 
@@ -253,7 +253,7 @@ tmp = roq.codec.fix.SecListGrp(symbol="BTC-PERPETUAL", security_exchange="deribi
 security_list = roq.codec.fix.SecurityList(
     security_req_id="req1",
     security_response_id="resp1",
-    security_request_result=roq.codec.fix.SecurityRequestResult.VALID,
+    security_request_result=roq.fix.SecurityRequestResult.VALID,
     no_related_sym=[tmp],
 )
 print("security_list={}".format(security_list))
